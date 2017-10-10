f66ccd3ea95ad6af67cc1a5d27649e5b5fa9885b
Amit Cirt <siamitcs0@gmail.com>
2017-10-10 19:44:08 +0300

    add read options to utils

--- io-utils/src/main/java/Utils/IoUtils/IoUtils.java
+++ io-utils/src/main/java/Utils/IoUtils/IoUtils.java
@@ -8,26 +8,37 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.function.BiConsumer;
 
-public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
-    private static final int EOF = -1;
+public class IoUtils {
+    public static final int NO_MAX_BYTE_TO_READ = -1;
+    static final int EOF = -1;
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
 
     public static long fileSize(final String path) {
         return new File(path).length();
     }
 
-    private static int read(InputStream input, byte[] data, int size) throws IOException {
+    private static int read0(InputStream input, byte[] data, int size) throws IOException {
         return input.read(data, 0, size);
     }
 
-    private static void write(OutputStream output, byte[] data, int size) throws IOException {
+    private static void write0(OutputStream output, byte[] data, int size) throws IOException {
         output.write(data, 0, size);
+    }
+
+    public static long read(InputStream input, byte[] data, int size) throws IOException {
+        int curDataPos = 0;
+        return read(input, size, new byte[DEFAULT_BUFFER_SIZE], (bytes, integer) -> System.arraycopy(bytes, 0, data, curDataPos, integer));
+    }
+
+    public static void write(OutputStream output, byte[] data, int size) throws IOException {
+        write0(output, data, size);
     }
 
     public static long read(InputStream input, byte[] data, BiConsumer<byte[], Integer> readHandler) throws IOException {
         final int dataSize = data.length;
         long count = 0;
         int curCount;
-        while (EOF != (curCount = read(input, data, dataSize))) {
+        while (EOF != (curCount = read0(input, data, dataSize))) {
             readHandler.accept(data, curCount);
             count += curCount;
         }
@@ -38,7 +49,7 @@ public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
         final int dataSize = data.length;
         long count = 0;
         int curCount;
-        while (EOF != (curCount = read(input, data, dataSize))) {
+        while (EOF != (curCount = read0(input, data, dataSize))) {
             readHandler.accept(data, curCount);
             count += curCount;
         }
@@ -46,7 +57,7 @@ public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
     }
 
     public static long read(InputStream input, long maxBytesToRead, byte[] data, BiConsumer<byte[], Integer> readHandler) throws IOException {
-        if (maxBytesToRead == -1) {
+        if (maxBytesToRead == NO_MAX_BYTE_TO_READ) {
             return read(input, data, readHandler);
         } else if (maxBytesToRead == 0) {
             return 0;
@@ -56,7 +67,7 @@ public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
         long count = 0;
         int curCount;
         do {
-            curCount = read(input, data, curMaxBytesToRead);
+            curCount = read0(input, data, curMaxBytesToRead);
             if (curCount == EOF) {
                 return count;
             }
@@ -68,7 +79,7 @@ public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
     }
 
     public static <T extends Exception> long readEx(InputStream input, long maxBytesToRead, byte[] data, BiConsumerException<byte[], Integer, T> readHandler) throws IOException, T {
-        if (maxBytesToRead == -1) {
+        if (maxBytesToRead == NO_MAX_BYTE_TO_READ) {
             return readEx(input, data, readHandler);
         } else if (maxBytesToRead == 0) {
             return 0;
@@ -78,7 +89,7 @@ public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
         long count = 0;
         int curCount;
         do {
-            curCount = read(input, data, curMaxBytesToRead);
+            curCount = read0(input, data, curMaxBytesToRead);
             if (curCount == EOF) {
                 return count;
             }
@@ -90,7 +101,7 @@ public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
     }
 
     public static long copy(InputStream input, OutputStream output, long maxBytesToCopy, byte[] data) throws IOException {
-        return readEx(input, maxBytesToCopy, data, (bytes, integer) -> write(output, bytes, integer));
+        return readEx(input, maxBytesToCopy, data, (bytes, integer) -> write0(output, bytes, integer));
     }
 
     public static long copy(InputStream input, OutputStream output, long maxBytesToCopy) throws IOException {
@@ -98,6 +109,6 @@ public class IoUtils {private static final int DEFAULT_BUFFER_SIZE = 1024 * 2;
     }
 
     public static long copy(InputStream input, OutputStream output) throws IOException {
-        return copy(input, output, -1, new byte[DEFAULT_BUFFER_SIZE]);
+        return copy(input, output, NO_MAX_BYTE_TO_READ, new byte[DEFAULT_BUFFER_SIZE]);
     }
 }
